1335878e79ab26aad5b6066e8a87c4fe8145fa83
Hoang Thanh Binh <hoangthanhbinh24092005@gmail.com>
2024-06-30 22:14:16 +0700

    master

--- models/Coach.java
+++ models/Coach.java
@@ -1,3 +1,26 @@
 public class Coach extends Person {
-    
+    private static int coachID ;
+    private int experience;
+    private int clubID;
+
+    public Coach(){}
+
+    public Coach (int coachID,String name,int age, String nationality,int experience, int clubID){
+        super(name, age, nationality); 
+        Coach.coachID = coachID;
+        this.experience = experience;
+        this.clubID = clubID;
+    }
+
+    public int getCoachID(){
+        return Coach.coachID;
+    }
+
+    public int getExperience(){
+        return this.experience;
+    }
+
+    public void setExperience(int experience){
+        this.experience = experience;
+    }
 }
